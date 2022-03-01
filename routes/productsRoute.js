7fae26753ea54b266ced821b131abe1f40564309
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-03-01 19:43:15 -0300

    feat: cria outras rotas de produtos

--- routes/productsRoute.js
+++ routes/productsRoute.js
@@ -1,9 +1,16 @@
 const router = require('express').Router();
-
 const controller = require('../controllers/products.controler');
+const productsValidationMiddleware = require('../middlewares/products.middleware');
 
 router.get('/', controller.getAll);
 
 router.get('/:id', controller.getById);
+
+router.post(
+  '/',
+  productsValidationMiddleware, controller.create,
+);
+
+router.put('/:id', productsValidationMiddleware, () => {});
 
 module.exports = router;
