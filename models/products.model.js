968c0d58d2b5dde5a8064b311d32f818a98c2b7b
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-03-01 19:42:55 -0300

    feat: implementa as finções getProductByName e create no model de produtos

--- models/products.model.js
+++ models/products.model.js
@@ -2,8 +2,8 @@ const connection = require('./connection');
 
 const getAll = async () => {
   const SQL_QUERY = 'SELECT * FROM StoreManager.products;';
-  const [products] = await connection.execute(SQL_QUERY);
-  return products;
+  const [productsList] = await connection.execute(SQL_QUERY);
+  return productsList;
 };
 
 const getById = async (id) => {
@@ -14,7 +14,34 @@ const getById = async (id) => {
   return productData[0];
 };
 
+const getProductByName = async (name) => {
+  const SQL_QUERY = 'SELECT * FROM StoreManager.products WHERE name = ?';
+
+  const [result] = await connection.execute(SQL_QUERY, [name]);
+
+  return result;
+};
+
+const create = async (name, quantity) => {
+  const SQL_QUERY = 'INSERT INTO StoreManager.products (name, quantity) VALUES (?, ?);';
+
+  const [newProduct] = await connection.execute(
+    SQL_QUERY,
+    [name, quantity],
+  );
+
+  const createdProduct = {
+    id: newProduct.insertId,
+    name,
+    quantity,
+  };
+
+  return createdProduct;
+};
+
 module.exports = {
   getAll,
   getById,
+  create,
+  getProductByName,
 };
