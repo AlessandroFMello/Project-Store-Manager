a44216ba65fdb58423d4a262e3ccc7b9e1ce7198
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-03-01 19:41:37 -0300

    feat: implementa a função create no controller de products

--- controllers/products.controler.js
+++ controllers/products.controler.js
@@ -1,15 +1,16 @@
-const products = require('../models/products.model');
+const productsModel = require('../models/products.model');
+const productsService = require('../services/products.service');
 
 module.exports = {
   getAll: async (_req, res) => {
-    const allProducts = await products.getAll();
+    const allProducts = await productsModel.getAll();
   
     return res.status(200).json(allProducts);
   },
   getById: async (req, res) => {
     const { id } = req.params;
 
-    const product = await products.getById(id);
+    const product = await productsModel.getById(id);
   
     if (!product) {
       return res
@@ -17,5 +18,15 @@ module.exports = {
     }
     
     return res.status(200).json(product);
+  },
+  create: async (req, res, _next) => {
+    const { code, message, product } = await productsService
+      .create(req.body);
+
+    if (!product) {
+      return res.status(code).json({ message });
+    }
+
+    return res.status(code).json(product);
   },
 };
