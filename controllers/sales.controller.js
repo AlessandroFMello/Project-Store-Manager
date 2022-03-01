cc7e3561519f6074cec13158547c93e72f7b6e07
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-03-01 19:45:08 -0300

    fix: corrige validações de sales

--- controllers/sales.controller.js
+++ controllers/sales.controller.js
@@ -2,16 +2,16 @@ const sales = require('../models/sales.model');
 
 module.exports = {
   getAll: async (_req, res) => {
-    const actors = await sales.getAll();
+    const allSales = await sales.getAll();
   
-    return res.status(200).json(actors);
+    return res.status(200).json(allSales);
   },
   getById: async (req, res) => {
     const { id } = req.params;
 
     const sale = await sales.getById(id);
   
-    if (!sale) {
+    if (!sale || sale.length === 0 || sale === []) {
       return res
         .status(404).json({ message: 'Sale not found' });
     }
