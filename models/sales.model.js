28fd6bab47e121a7e5c0684f015babfce7129473
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-03-02 14:09:09 -0300

    feat implementa a função create da camada de model de vendas

--- models/sales.model.js
+++ models/sales.model.js
@@ -1,28 +1,38 @@
 const connection = require('./connection');
 
-const getAll = async () => {
-  const SQL_QUERY = `SELECT sale_id AS saleId, date, product_id AS productId, quantity 
-  FROM StoreManager.sales_products AS salesProducts 
-  INNER JOIN StoreManager.sales AS sales 
-  ON sales.id = salesProducts.sale_id;`;
-  const [sales] = await connection.execute(SQL_QUERY);
-  return sales;
-};
-
-const getById = async (id) => {
-  const SQL_QUERY = `SELECT date, product_id AS productId, quantity 
-  FROM StoreManager.sales_products AS salesProducts 
-  INNER JOIN StoreManager.sales AS sales 
-  ON sales.id = salesProducts.sale_id WHERE id = ?;`;
-  const [saleData] = await connection.execute(
-    SQL_QUERY,
-    [id],
-    );
-
-  return saleData;
-};
-
 module.exports = {
-  getAll,
-  getById,
+  getAll: async () => {
+    const SQL_QUERY = `SELECT sale_id AS saleId, date, product_id AS productId, quantity 
+    FROM StoreManager.sales_products AS salesProducts 
+    INNER JOIN StoreManager.sales AS sales 
+    ON sales.id = salesProducts.sale_id;`;
+    const [sales] = await connection.execute(SQL_QUERY);
+    return sales;
+  },  
+  getById: async (id) => {
+    const SQL_QUERY = `SELECT date, product_id AS productId, quantity 
+    FROM StoreManager.sales_products AS salesProducts 
+    INNER JOIN StoreManager.sales AS sales 
+    ON sales.id = salesProducts.sale_id WHERE id = ?;`;
+    const [saleData] = await connection.execute(
+      SQL_QUERY,
+      [id],
+      );
+  
+    return saleData;
+  },
+  createId: async () => {
+    const SQL_QUERY = 'INSERT INTO StoreManager.sales () VALUES ();';
+    const [sale] = await connection.execute(SQL_QUERY);
+
+    return sale.insertId;
+  },  
+  create: async (saleId, productId, quantity) => {
+    const SQL_QUERY = `INSERT INTO StoreManager.sales_products (sale_id, product_id, quantity)
+    VALUES (?, ?, ?)`;
+    await connection.execute(
+      SQL_QUERY,
+      [saleId, productId, quantity],
+    );
+  },
 };
