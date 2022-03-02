f812bab1a68d391f586bc62a103b6d45cc35445d
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-03-02 14:09:29 -0300

    feat implementa a função create da camada de controller de vendas

--- controllers/sales.controller.js
+++ controllers/sales.controller.js
@@ -1,21 +1,27 @@
-const sales = require('../models/sales.model');
+const salesModel = require('../models/sales.model');
+const salesService = require('../services/sales.service');
 
 module.exports = {
   getAll: async (_req, res) => {
-    const allSales = await sales.getAll();
+    const allSales = await salesModel.getAll();
   
     return res.status(200).json(allSales);
   },
   getById: async (req, res) => {
     const { id } = req.params;
 
-    const sale = await sales.getById(id);
+    const saleById = await salesModel.getById(id);
   
-    if (!sale || sale.length === 0 || sale === []) {
+    if (!saleById || saleById.length === 0 || saleById === []) {
       return res
         .status(404).json({ message: 'Sale not found' });
     }
     
-    return res.status(200).json(sale);
+    return res.status(200).json(saleById);
+  },
+  create: async (req, res) => {
+    const { code, sale } = await salesService.create(req.body);
+  
+    return res.status(code).json(sale);
   },
 };
