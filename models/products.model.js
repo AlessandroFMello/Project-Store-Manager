1bf0db08d77d6961fb0a3dc21df3b3c9069340c0
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-03-01 22:39:36 -0300

    fix: refatora products.model

--- models/products.model.js
+++ models/products.model.js
@@ -1,61 +1,51 @@
 const connection = require('./connection');
 
-const getAll = async () => {
-  const SQL_QUERY = 'SELECT * FROM StoreManager.products;';
-  const [productsList] = await connection.execute(SQL_QUERY);
-  return productsList;
-};
-
-const getById = async (id) => {
-  const SQL_QUERY = 'SELECT * FROM StoreManager.products WHERE id = ?;';
-  
-  const [productData] = await connection.execute(SQL_QUERY, [id]);
-
-  return productData[0];
-};
-
-const getProductByName = async (name) => {
-  const SQL_QUERY = 'SELECT * FROM StoreManager.products WHERE name = ?';
-
-  const [result] = await connection.execute(SQL_QUERY, [name]);
-
-  return result;
-};
-
-const create = async (name, quantity) => {
-  const SQL_QUERY = 'INSERT INTO StoreManager.products (name, quantity) VALUES (?, ?);';
-
-  const [newProduct] = await connection.execute(
-    SQL_QUERY,
-    [name, quantity],
-  );
-
-  const createdProduct = {
-    id: newProduct.insertId,
-    name,
-    quantity,
-  };
-
-  return createdProduct;
-};
-
-const update = async (id, name, quantity) => {
-  const SQL_QUERY = 'UPDATE StoreManager.products SET name = ?, quantity = ? WHERE id = ?;';
-  const [productToUpdate] = await connection.execute(
-    SQL_QUERY,
-    [name, quantity, id],
-  );
-
-  if (productToUpdate) {
-    const updateProduct = { id, name, quantity };
-    return updateProduct;
-  }
-};
-
 module.exports = {
-  getAll,
-  getById,
-  create,
-  getProductByName,
-  update,
+  getAll: async () => {
+    const SQL_QUERY = 'SELECT * FROM StoreManager.products;';
+    const [productsList] = await connection.execute(SQL_QUERY);
+    return productsList;
+  },
+  getById: async (id) => {
+    const SQL_QUERY = 'SELECT * FROM StoreManager.products WHERE id = ?;';
+    
+    const [productData] = await connection.execute(SQL_QUERY, [id]);
+  
+    return productData[0];
+  },
+  getProductByName: async (name) => {
+    const SQL_QUERY = 'SELECT * FROM StoreManager.products WHERE name = ?';
+  
+    const [result] = await connection.execute(SQL_QUERY, [name]);
+  
+    return result;
+  },
+  create: async (name, quantity) => {
+    const SQL_QUERY = 'INSERT INTO StoreManager.products (name, quantity) VALUES (?, ?);';
+  
+    const [newProduct] = await connection.execute(
+      SQL_QUERY,
+      [name, quantity],
+    );
+  
+    const createdProduct = {
+      id: newProduct.insertId,
+      name,
+      quantity,
+    };
+  
+    return createdProduct;
+  },
+  update: async (id, name, quantity) => {
+    const SQL_QUERY = 'UPDATE StoreManager.products SET name = ?, quantity = ? WHERE id = ?;';
+    const [productToUpdate] = await connection.execute(
+      SQL_QUERY,
+      [name, quantity, id],
+    );
+  
+    if (productToUpdate) {
+      const updateProduct = { id, name, quantity };
+      return updateProduct;
+    }
+  },
 };
